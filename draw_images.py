3ccba1e012e64275dce740189f1e5e0c8b778d54
developeravsk <readeravskh@gmail.com>
2022-04-26 15:00:39 +0545

    Draw stuff on images

--- draw_images.py
+++ draw_images.py
@@ -11,7 +11,21 @@ blank[:]=255,0,0
 
 # draw rectangle
 # parameters-> Initial point, ending point, color, thickness
-cv.rectangle(blank,(0,0),(250,250), (0,255,0), thickness=2)
+# cv.FILLED or -1
+# cv.rectangle(blank,(0,0),(250,250), (0,255,0), thickness=cv.FILLED)
+
+# scaled the rectangle to half of original image
+cv.rectangle(blank,(0,0),(blank.shape[1]//2,blank.shape[0]//2), (0,255,0), thickness=cv.FILLED)
+
+
+# draw circle
+cv.circle(blank, center=(blank.shape[1]//2,blank.shape[0]//2), radius=40, color=(0,0,255), thickness=3)
+
+# draw line
+cv.line(blank, pt1=(0,0), pt2=(blank.shape[1]//2,blank.shape[0]//2), color=(255,255,255),thickness=3)
+
+# write text on image
+cv.putText(blank,text="Hello there",org=(400,400), fontFace=cv.FONT_HERSHEY_TRIPLEX, fontScale=1.0, color=(0,255,0),thickness=2)
 
 # show in window
 cv.imshow('Blank', blank)
